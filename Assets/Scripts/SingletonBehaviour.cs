c24e961fa67ef9f62c8783e1338b67e7682fc49b
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-05-04 20:32:45 -0600

    setup project as a Unity package

---
+++ Assets/Scripts/SingletonBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Tofunaut.TofuUnity
+{
+    /// <summary>
+    /// An implementation of a Singleton MonoBehviour.
+    /// </summary>
+    public abstract class SingletonBehaviour<T> : MonoBehaviour where T: MonoBehaviour
+    {
+
+        public static bool HasInstance => _instance != null;
+
+        protected static T _instance;
+
+        protected virtual bool SetDontDestroyOnLoad => false;
+
+        protected virtual void Awake()
+        {
+            if(_instance != null)
+            {
+                Debug.LogErrorFormat("another instance of {0} already exists, this one will be destroyed", nameof(T));
+                return;
+            }
+
+            _instance = this as T;
+
+            if(SetDontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(_instance);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if(_instance == this)
+            {
+                _instance = null;
+            }
+        }
+    }
+}
