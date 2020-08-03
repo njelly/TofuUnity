7c08046014a56ba0a405b9ddb292652994283c88
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-08-02 18:08:57 -0600

    fixed namespaces

--- Runtime/Vector2IntQuadTree.cs
+++ Runtime/Vector2IntQuadTree.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Tofunaut.TofuUnity.Samples.QuadTree
+namespace Tofunaut.TofuUnity
 {
     public class Vector2IntQuadTree<T>
     {
