3f5771c2baa3007e20e79aae2386bebd691c02cd
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-08-02 22:57:02 -0600

    added SpriteOrderInLayerByY.cs

---
+++ Runtime/SpriteOrderInLayerByY.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Tofunaut.TofuUnity
+{
+    public class SpriteOrderInLayerByY : MonoBehaviour
+    {
+        public SpriteRenderer spriteRenderer;
+        public bool invert;
+
+        private void LateUpdate()
+        {
+            spriteRenderer.sortingOrder = Mathf.CeilToInt(transform.position.y);
+
+            if (invert)
+            {
+                spriteRenderer.sortingOrder *= -1;
+            }
+        }
+    }
+}
