8093d701904e9697fde7bd121ae8457fb65e25e1
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-09-17 09:22:30 -0600

    added TofuAppVersionProvider

---
+++ Runtime/TofuAppVersionProvider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Tofunaut.TofuUnity
+{
+    public class TofuAppVersionProvider : MonoBehaviour, ITofuVersionProvider
+    {
+        public TofuVersion Version => _version;
+
+        private TofuVersion _version;
+
+        private void Awake()
+        {
+            _version = new TofuVersion($"{Application.version}{TofuVersion.Delimiter}{BuildNumberUtil.ReadBuildNumber()}");
+        }
+    }
+}
