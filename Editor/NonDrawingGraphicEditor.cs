832753cf79f2dd80d5ddb3c427d27774909a4c37
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-09-26 17:42:38 -0600

    added NonDrawingGraphicEditor, removed RequireInterfaceDrawer

---
+++ Editor/NonDrawingGraphicEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace Tofunaut.TofuUnity
+{
+    /// <summary>
+    /// Based on the implementation described at:
+    /// https://answers.unity.com/questions/1091618/ui-panel-without-image-component-as-raycast-target.html
+    /// </summary>
+    [CanEditMultipleObjects, CustomEditor(typeof(NonDrawingGraphic), false)]
+    public class NonDrawingGraphicEditor : GraphicEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.serializedObject.Update();
+            EditorGUILayout.PropertyField(base.m_Script, new GUILayoutOption[0]);
+            base.RaycastControlsGUI();
+            base.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Runtime/NonDrawingGraphic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+
+namespace Tofunaut.TofuUnity
+{
+    /// <summary>
+    /// A non-drawing graphic is an efficient way to add the ability to hit a UI element that does not need any drawn graphics. Like an input rect.
+    /// Based on the implementation described at:
+    /// https://answers.unity.com/questions/1091618/ui-panel-without-image-component-as-raycast-target.html
+    /// </summary>
+    public class NonDrawingGraphic : Graphic
+    {
+        public override void SetMaterialDirty() { }
+        public override void SetVerticesDirty() { }
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+        }
+    }
+}
