f5764b81dc89e2b3ca718a3965f3dae92e6b8bc2
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-10-29 23:13:31 -0600

    fixed loglevels

--- Runtime/Interfaces/ILogger.cs
+++ Runtime/Interfaces/ILogger.cs
@@ -1,7 +1,18 @@
 ﻿namespace Tofunaut.TofuUnity
 {
+    public enum LogLevel
+    {
+        All = int.MinValue,
+        Info = 0,
+        Warn = 1,
+        Error = 2,
+        Exception = 3,
+        None = int.MinValue,
+    }
+
     public interface ILogger
     {
+
         void Info(string s);
         void Warn(string s);
         void Error(string s);
