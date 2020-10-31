6c617e53d36e60a224193d498814cb3132c2b3ba
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-10-31 12:52:44 -0600

    added HasLength to VectorExtensions

--- Runtime/VectorExtentions.cs
+++ Runtime/VectorExtentions.cs
@@ -6,18 +6,24 @@ namespace Tofunaut.TofuUnity
     {
         public static ECardinalDirection4 ToCardinalDirection4(this Vector2 v)
         {
-            float angle = Mathf.Atan2(v.y, v.x);
-            int quarter = Mathf.RoundToInt(Mathf.Round(4 * (angle / (Mathf.PI * 2f)) + 4) % 4);
+            var angle = Mathf.Atan2(v.y, v.x);
+            var quarter = Mathf.RoundToInt(Mathf.Round(4 * (angle / (Mathf.PI * 2f)) + 4) % 4);
 
             return (ECardinalDirection4)quarter;
         }
 
         public static ECardinalDirection8 ToCardinalDirection8(this Vector2 v)
         {
-            float angle = Mathf.Atan2(v.y, v.x);
-            int octant = Mathf.RoundToInt(Mathf.Round(8 * (angle / (Mathf.PI * 2f)) + 8) % 8);
+            var angle = Mathf.Atan2(v.y, v.x);
+            var octant = Mathf.RoundToInt(Mathf.Round(8 * (angle / (Mathf.PI * 2f)) + 8) % 8);
 
             return (ECardinalDirection8)octant;
         }
+
+        public static bool HasLength(this Vector3 v) => 
+            Mathf.Abs(v.x) > float.Epsilon || Mathf.Abs(v.y) > float.Epsilon || Mathf.Abs(v.z) > float.Epsilon;
+
+        public static bool HasLength(this Vector2 v) =>
+            Mathf.Abs(v.x) > float.Epsilon || Mathf.Abs(v.y) > float.Epsilon;
     }
 }
