98e14079ced2992a56862551d2d657e5c48d1c99
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-11-21 11:24:13 -0700

    created MathfUtils and MathfUtilsTests

---
+++ Runtime/MathfUtils.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Tofunaut.TofuUnity
+{
+    public static class MathfUtils
+    {
+        public static float SmallestAngleDifferenceDeg(float fromDeg, float toDeg)
+        {
+            var diff = (toDeg - fromDeg + 180f) % 360f - 180f;
+            return diff < -180f ? diff + 360f : diff;
+        }
+        
+        public static float SmallestAngleDifferenceRad(float fromRad, float toRad)
+        {
+            return SmallestAngleDifferenceDeg(fromRad * Mathf.Rad2Deg, toRad * Mathf.Rad2Deg) * Mathf.Deg2Rad;
+        }
+    }
+}
---
+++ Tests/Runtime/MathfUtilsTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Tofunaut.TofuUnity;
+using UnityEngine;
+
+namespace Tests.Runtime
+{
+    public class MathfUtilsTests
+    {
+        [Test]
+        public void TestSmallestAngleDiff()
+        {
+            const float fromDeg = 350f;
+            const float toDeg = 10f;
+            
+            Assert.IsTrue(MathfUtils.SmallestAngleDifferenceDeg(fromDeg, toDeg).IsApproximately(20f));
+
+            const float fromRad = fromDeg * Mathf.Deg2Rad;
+            const float toRad = toDeg * Mathf.Deg2Rad;
+            
+            Assert.IsTrue(MathfUtils.SmallestAngleDifferenceRad(fromRad, toRad).IsApproximately(20f * Mathf.Deg2Rad));
+        }
+    }
+}
