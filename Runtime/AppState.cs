b59bdc0a0c9707484054f3717b22690b2bae32de
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-11-30 22:16:44 -0700

    added AppState and AppStateController

---
+++ Runtime/AppState.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Tofunaut.TofuUnity
+{
+    public class AppState<T> where T : AppStateController<T>
+    {
+        public bool IsComplete => _stateController && _stateController.IsReady && _stateController.IsComplete;
+
+        private readonly int _sceneIndex;
+
+        private T _stateController;
+
+        public AppState(int sceneIndex)
+        {
+            _sceneIndex = sceneIndex;
+        }
+
+        public virtual async Task Enter()
+        {
+            var loadTask = SceneManager.LoadSceneAsync(_sceneIndex);
+            while (!loadTask.isDone)
+                await Task.Yield();
+
+            _stateController = Object.FindObjectOfType<T>();
+            if(!_stateController)
+                Debug.LogError($"no object found of type {nameof(T)}");
+        }
+    }
+}
---
+++ Runtime/AppStateController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Tofunaut.TofuUnity
+{
+    public abstract class AppStateController<T> : SingletonBehaviour<T> where T : MonoBehaviour
+    {
+        public bool IsComplete { get; private set; }
+        public bool IsReady { get; protected set; }
+
+        public void Complete()
+        {
+            IsComplete = true;
+        }
+    }
+}
