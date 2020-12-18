4b21435e148e0bc10f636fb0d53b3ece0718dd9a
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-12-17 18:43:47 -0700

    created TextImportWindow

---
+++ Editor/TextImportWindow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Tofunaut.TofuUnity.Editor
+{
+    public class TextImportWindow : EditorWindow
+    {
+        private Action<string> _onComplete;
+        private Vector2 _scrollPos;
+        private string _text;
+        
+        public static void Init(string title, Action<string> onComplete)
+        {
+            var window = (TextImportWindow) EditorWindow.GetWindow(typeof(TextImportWindow));
+            window.titleContent = new GUIContent(title);
+            window._onComplete = onComplete;
+            window._text = string.Empty;
+        }
+
+        public void OnGUI()
+        {
+            if (GUILayout.Button("Open File"))
+                ImportFromFile();
+            
+            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUILayout.ExpandHeight(true));
+            EditorGUILayout.TextArea(_text, GUILayout.ExpandHeight(true));
+            EditorGUILayout.EndScrollView();
+            
+            if (GUILayout.Button("Import"))
+                _onComplete?.Invoke(_text);
+        }
+
+        private void ImportFromFile()
+        {
+            var path = EditorUtility.OpenFilePanel("Open", "", "txt,json,csv");
+            if (path.Length != 0)
+                _text =  File.ReadAllText(path);
+        }
+    }
+}
