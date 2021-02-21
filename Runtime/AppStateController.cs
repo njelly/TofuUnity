0b9a2603ac1f8aba4ea9c428c8ec16c51cf14bed
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2021-02-21 11:34:12 -0700

    created TofuRandomState

--- Runtime/AppStateController.cs
+++ Runtime/AppStateController.cs
@@ -6,6 +6,9 @@ namespace Tofunaut.TofuUnity
     
     public abstract class AppStateController<T1, T2> : SingletonBehaviour<T1> where T1 : MonoBehaviour where T2 : IAppStateModel
     {
+        public static bool IsInstanceComplete => HasInstance && (_instance as AppStateController<T1, T2>).IsComplete;
+        public static bool IsInstanceReady => HasInstance && (_instance as AppStateController<T1, T2>).IsReady;
+        
         public bool IsComplete { get; private set; }
         public bool IsReady { get; protected set; }
         
---
+++ Runtime/TofuRandomState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Tofunaut.TofuUnity
+{
+    /// <summary>
+    /// Keeps track of the last value of System.Random so the sequence of the RNG can be resumed without having to
+    /// regenerate the previous values.
+    /// </summary>
+    [Serializable]
+    public class TofuRandomState
+    {
+        private int _seed;
+        private int _lastValue;
+
+        public TofuRandomState(int seed, int lastValue = 0)
+        {
+            _seed = seed;
+            _lastValue = lastValue;
+        }
+
+        public int Next()
+        {
+            _lastValue = new Random(_seed + _lastValue).Next();
+            return _lastValue;
+        }
+
+        public float NextFloat() => Range(0f, 1f);
+
+        public float Range(float min, float max)
+        {
+            var n = Next();
+            var ratio = (float)n / int.MaxValue;
+            return (max - min) * ratio + min;
+        }
+
+        public int Range(int min, int max) => (int) Range((float) min, max);
+    }
+}
